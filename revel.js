cb8a6ba05661e66541aabe23462a1fe2a90a741e
Carlos Piquet <156472451+CarlosPiquetEngdeS@users.noreply.github.com>
2024-01-30 23:48:06 -0300

    teste 02

---
+++ revel.js
@@ -0,0 +1,110 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO DO SITE //
+revelar.reveal('.efeito-txt-topo',
+{
+    duration: 2000,
+    distance: '90px'
+
+})
+
+revelar.reveal('.efeito-img-topo',
+{
+    duration: 2000,
+    distance: '90px', 
+    delay: 500
+
+})
+
+// OS TITULOS //
+
+revelar.reveal('.titulo',
+{
+    duration: 2000,
+    distance: '90px'
+    
+})
+
+
+
+revelar.reveal('.efeito-especialidades1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+    
+})
+
+revelar.reveal('.efeito-especialidades2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+    
+})
+
+revelar.reveal('.efeito-especialidades3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+    
+})
+
+revelar.reveal('.efeito-img-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin:'left'
+    
+})
+
+revelar.reveal('.efeito-txt-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin:'right'
+    
+})
+
+revelar.reveal('.efeito-portifolio1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin:'top'
+    
+})
+
+revelar.reveal('.efeito-portifolio2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500,
+    origin:'right'
+    
+})
+
+revelar.reveal('.efeito-portifolio3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 2000,
+    origin:'right'
+    
+})
+
+revelar.reveal('.efeito-interface',
+{
+    duration: 2000,
+    distance: '100px',
+    delay: 2000,
+    origin:'left'
+    
+})
+
+
+
